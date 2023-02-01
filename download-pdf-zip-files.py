c81c1cc5352df0ab6c814f5d56ba844f03c9f51e
carloslme <32799711+carloslme@users.noreply.github.com>
2023-01-31 21:23:35 -0600

    feat: refactor pdf and zip files

--- download-pdf-zip-files.py
+++ download-pdf-zip-files.py
@@ -2,6 +2,27 @@ from bs4 import BeautifulSoup
 import requests
 import shutil
 
+def download_file(link_str: str):
+    book_name = link_str.get('href')
+    full_link = main_link + book_name
+    
+    print(f"Downloading file number: {full_link}")
+    
+    # Get response object for link
+    response = requests.get(full_link)
+    
+    # Write content in pdf file
+    with open(f"{book_name}", 'wb') as file:
+        file.write(response.content)
+
+    print(f"File {book_name} downloaded")
+    
+
+def process_zip_file(book_name: str):
+    # Unzip the file
+    shutil.unpack_archive(book_name, "./")
+    print(f"File: {book_name} unzipped.")
+
 main_link = 'https://porn.jules-aubert.info/humble_bundle/applied_math_productivity_by_mercury/'
 
 # Making a GET request
@@ -11,53 +32,17 @@ soup = BeautifulSoup(r.text, 'html.parser')
 
 # Find all hyperlinks present on webpage
 links = soup.find_all('a')
-#print(links)
-i = 0
-  
+
+file_types = [".zip", ".pdf"]
+
 # From all links check for pdf link and
 # if present download file
 for link in links:
-    # Uncomment the following line to process 
-    # an specific number of files
-    if i == 3:
-        break
-     
-    if ('.pdf' in link.get('href', [])):
-        
-        book_name = link.get('href')
-        full_link = main_link + book_name
-        
-        print(f"Downloading file number {i+1}: {full_link}")
-        
-        # Get response object for link
-        response = requests.get(full_link)
+    href = link.get('href', [])
+    book_name = link.get('href')
 
-        # Write content in pdf file
-        with open(f"{book_name}.pdf", 'wb') as pdf:
-            pdf.write(response.content)
-
-        print(f"File {i+1}: {book_name} downloaded")
-        
-        i += 1
+    if file_types[0] in href or file_types[1] in href:
+        download_file(link_str=link)
     
-    # Process zip files
-    if ('.zip' in link.get('href', [])):
-        
-        book_name = link.get('href')
-        full_link = main_link + book_name
-        
-        print(f"Downloading file number {i+1}: {full_link}")
-        
-        # Get response object for link
-        response = requests.get(full_link)
-
-        # Write content in pdf file
-        with open(f"{book_name}", 'wb') as pdf:
-            pdf.write(response.content)
-
-        print(f"File {i+1}: {book_name} downloaded")
-        
-        # Unzip the file
-        shutil.unpack_archive(book_name, "./")
-        print(f"File {i+1}: {book_name} unzipped.")
-        i += 1
+    if file_types[0] in href:
+        process_zip_file(book_name)

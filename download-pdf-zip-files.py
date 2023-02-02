af1af5456d24f72a65da44365d81ccfa568b2c42
carloslme <32799711+carloslme@users.noreply.github.com>
2023-02-01 22:42:17 -0600

    feat: add exception catching

--- download-pdf-zip-files.py
+++ download-pdf-zip-files.py
@@ -2,26 +2,34 @@ from bs4 import BeautifulSoup
 import requests
 import shutil
 
-def download_file(link_str: str):
-    book_name = link_str.get('href')
-    full_link = main_link + book_name
+def download_file(link_str: any):
+    try:
+        book_name = link_str.get('href')
+        full_link = main_link + book_name
+        
+        print(f"Downloading file number: {full_link}")
+        
+        # Get response object for link
+        response = requests.get(full_link)
+        
+        # Write content in pdf file
+        with open(f"{book_name}", 'wb') as file:
+            file.write(response.content)
     
-    print(f"Downloading file number: {full_link}")
-    
-    # Get response object for link
-    response = requests.get(full_link)
-    
-    # Write content in pdf file
-    with open(f"{book_name}", 'wb') as file:
-        file.write(response.content)
+        return True
 
-    print(f"File {book_name} downloaded")
-    
+    except Exception as err:
+        #print(f"Unexpected {err}, {type(err)}")
+        return err
+
 
 def process_zip_file(book_name: str):
-    # Unzip the file
-    shutil.unpack_archive(book_name, "./")
-    print(f"File: {book_name} unzipped.")
+    try:
+        # Unzip the file
+        shutil.unpack_archive(book_name, "./")
+        return True
+    except Exception as err:
+        return err
 
 main_link = 'https://porn.jules-aubert.info/humble_bundle/applied_math_productivity_by_mercury/'
 
@@ -37,12 +45,21 @@ file_types = [".zip", ".pdf"]
 
 # From all links check for pdf link and
 # if present download file
-for link in links:
+for link in links[6:7]:
     href = link.get('href', [])
     book_name = link.get('href')
 
     if file_types[0] in href or file_types[1] in href:
-        download_file(link_str=link)
-    
-    if file_types[0] in href:
-        process_zip_file(book_name)
+        result = download_file(link_str=link)
+        if result == True:
+            print(f"File {book_name} downloaded")
+            
+            # If file is zip, then unzip it
+            if file_types[0] in href:
+                result_unzip = process_zip_file(book_name)
+                if result_unzip == True:
+                    print(f"File: {book_name} unzipped.")
+                else:
+                    print(f"Error while unzipping the book {book_name}. Please contact your admin.")
+        else:
+            print(f"Error while downloading the book {book_name}. Please contact your admin.")
